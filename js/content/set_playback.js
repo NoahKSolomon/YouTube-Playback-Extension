84dfa29d227d6d2e65c9c3da35001a7a1b9d721e
Noah K Solomon <noahksolomon@gmail.com>
2020-03-27 11:24:36 -0700

    Fixed content script bug

--- js/content/set_playback.js
+++ js/content/set_playback.js
@@ -1,8 +1,8 @@
 // Check for existence of video tags to set playback of upon initial injection
 if(!(document.getElementsByTagName("video")[0] === undefined)) {
     chrome.storage.sync.get('playback', data => {
-        if(speed != NaN && speed > 0) {
-            document.getElementsByTagName("video")[0].playbackRate = speed;
+        if(data.playback != NaN && data.playback > 0) {
+            document.getElementsByTagName("video")[0].playbackRate = data.playback;
         }
     });
 }
