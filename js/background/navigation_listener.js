99965021c89ebc34787f8c6984de1142d91047a7
Noah K Solomon <noahksolomon@gmail.com>
2020-03-30 18:29:09 -0700

    Removed unnecessarry print statements

--- js/background/navigation_listener.js
+++ js/background/navigation_listener.js
@@ -1,29 +1,22 @@
 var tab_list = new Array();
 
 chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
-    console.log("onUpdated fired");
-    console.log(changeInfo);
-    console.log(tab);
     if("url" in changeInfo) { // Check for url
         console.log("changeInfo has url: " + changeInfo.url);
         let cont_yt = changeInfo.url.search("youtube.com/watch");
         if(cont_yt !== -1) { // Check for updated url to be to a YouTube video
-            console.log("url contains youtube.com/watch at position: " + cont_yt);
             if("status" in changeInfo) {
                 if(changeInfo.status === "loading") { // If loading, store in list to inject on complete
                     tab_list.push(tabId);
-                    console.log("Saving to list: " + tabId + " --> " + changeInfo.url);
                 } else if(changeInfo.status === "complete") { // If completed, inject script
                     chrome.tabs.executeScript(tabId, {
                         file:"/js/content/set_playback.js"
                     });
-                    console.log("Sent content script to tab");
                 }
             }
         }
     } else if("status" in changeInfo && changeInfo.status === "complete") {
         if(tab_list.includes(tabId)) { // Tab waiting for content script
-            console.log("Injectting script into tab " + tabId + " from map");
             chrome.tabs.executeScript(tabId, {
                 file:"/js/content/set_playback.js"
             });
--- js/content/set_playback.js
+++ js/content/set_playback.js
@@ -1,13 +1,10 @@
 
 // Check for existence of video tags to set playback of upon initial injection
 if(!(document.getElementsByTagName("video")[0] === undefined)) {
-    console.log("Found video element");
     injected_script = true;
     chrome.storage.sync.get('playback', data => {
         if(data.playback != NaN && data.playback > 0) {
-            console.log("Setting video playback to " + data.playback);
             document.getElementsByTagName("video")[0].playbackRate = data.playback;
         }
     });
 }
-console.log("Content script executed");
